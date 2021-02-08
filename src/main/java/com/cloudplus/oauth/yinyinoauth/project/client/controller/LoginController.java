bd8dd9fe5cd67c804e37dd0cabe9b4b6df8df45f
张卢斌 <985851103@qq.com>
2021-02-08 14:15:32 +0800

    登录增加验证码，优化登录错误提示（未输入用户名或密码，验证码错误、用户名或密码错误）

--- src/main/java/com/cloudplus/oauth/yinyinoauth/project/client/controller/LoginController.java
+++ src/main/java/com/cloudplus/oauth/yinyinoauth/project/client/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.cloudplus.oauth.yinyinoauth.project.client.controller;
 
+import com.wf.captcha.SpecCaptcha;
+import com.wf.captcha.base.Captcha;
+import com.wf.captcha.utils.CaptchaUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
@@ -31,5 +37,33 @@ public class LoginController {
         request.getContextPath();
         model.getAttribute("aaa");
         return "401";
+    }
+
+    //生成自定义验证码
+    @RequestMapping("/captcha")
+    public void captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 三个参数分别为宽、高、位数
+        SpecCaptcha specCaptcha = new SpecCaptcha(130, 48, 5);
+        // 设置类型，纯数字、纯字母、字母数字混合
+        specCaptcha.setCharType(Captcha.TYPE_ONLY_NUMBER);
+
+        // 验证码存入session
+        request.getSession().setAttribute("captcha", specCaptcha.text().toLowerCase());
+
+        // 输出图片流
+        specCaptcha.out(response.getOutputStream());
+    }
+
+    @RequestMapping("/checkVerCode")
+    @ResponseBody
+    public int checkVerCode(String verCode, HttpServletRequest request){
+        // 获取session中的验证码
+        String sessionCode = (String) request.getSession().getAttribute("captcha");
+        // 判断验证码
+        if (verCode==null || !sessionCode.equals(verCode.trim().toLowerCase())) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
